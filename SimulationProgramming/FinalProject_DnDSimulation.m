7b62f7a62fe22b3f83d63fc12a2ba8d4144e0801
Alyssa Vellucci <amv170001@utdallas.edu>
2023-04-11 12:37:49 -0500

    Fixed matrices

--- SimulationProgramming/FinalProject_DnDSimulation.m
+++ SimulationProgramming/FinalProject_DnDSimulation.m
@@ -4,58 +4,6 @@ clear all; close all; %clc;
 % Alyssa Vellucci: AMV170001
 % Jonas Wagner: JRW200000
 
-%% Problem Definition
-melee = 1;
-ranged = 2;
-% nothing = 3;
-% heal = 4;
-heal = 3;
-nothing = 4; % switched to match the theory doc
-
-% Monster Set-Up
-m_act = [melee ranged nothing]; %actions monster can perform on its turn
-m_HPtot = 100; %monster hit point total
-m_HP = [0:1:m_HPtot]; %monster state vector
-
-%PC Set-Up
-pc_act = [melee ranged nothing heal]; %actions player character can perform on its turn
-pc_HPtot = m_HPtot; %player hit point total
-pc_HP = [0:1:pc_HPtot]; %pc state vector
-
-%Movement Inputs
-moveset.left = [-1 0]; moveset.right = [1,0]; moveset.up = [0 1]; moveset.down = [0 -1];
-moveset.upleft = [-1 1]; moveset.upright = [1 1]; moveset.downleft = [-1 -1]; moveset.downright = [1 -1];
-
-% left = [-1 0]; right = [1,0]; up = [0 1]; down = [0 -1];
-% upleft = [-1 1]; upright = [1 1]; downleft = [-1 -1]; downright = [1 -1];
-
-%moveset = {left, right, up, down, upleft, upright, downleft, downright};
-
-%Battlefield Set-Up
-battlefield_size = 1000;
-grid = zeros(battlefield_size, battlefield_size); %create battlefield
-
-%Potion Set-Up
-potion = [1 0]; %to check if potion is available
-
-%Dice Roll Set-Up
-d2 = [0.5 0.5];
-d4 = [0.25 0.25 0.25 0.25];
-d6 = [1/6 1/6 1/6 1/6 1/6 1/6];
-d8 = [1/8 1/8 1/8 1/8 1/8 1/8 1/8 1/8];
-d10 = [0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1 0.1];
-d20 = zeros(1,20);
-d100 = zeros(1,100);
-for i = 1:20
-    d20(i) = 1/20;
-end
-for i = 1:100
-    d100(i) = 1/100;
-end
-dice = {d2 d4 d6 d8 d10 d20 d100}; 
-
-
-
 %% System Parameters
 const.pc.melee.range = 1;
 const.pc.melee.weapon = 2;
@@ -71,6 +19,9 @@ const.mn = const.pc; % same stats
 const.pc.heal.baseheal = 1;
 const.pc.heal.d = 4;
 
+%Battlefield Set-Up
+battlefield_size = 50;
+grid = zeros(battlefield_size, battlefield_size); %create battlefield
 
 %% Markov Chain Definitions
 % Move 
@@ -143,16 +94,18 @@ M.pc.ranged = M.pc.ranged';
 % Heal
 % M.pc.heal.amount = @(w_d4) const.potion.baseheal + w_d4;
 M.pc.heal = zeros(num_hp_states);
-for j = 1:num_hp_states
+M.pc.heal(1,1) = 1;
+for j = 2:num_hp_states
     for d = 1:const.pc.heal.d
         i = j + const.pc.heal.baseheal + d;
         if i > num_hp_states; i = num_hp_states;end
         M.pc.heal(i,j) = M.pc.heal(i,j) + (1/const.pc.heal.d);
     end
 end
+M.pc.heal = M.pc.heal';
 
 % Nothing
-M.pc.nothing.markov = eye(num_hp_states);
+M.pc.nothing = eye(num_hp_states);
 
 % Monster
 M.mn = M.pc; % same attack assumption (could redefine to be different)
@@ -213,9 +166,26 @@ end
 
 % End of update
 
+%% Dynamic Programming
+% Infinite Horizon, Value Iteration
 
+%Initialize optimal costs, policies for movement
+J.move = zeros(battlefield_size, battlefield_size);
+pi_star.move = J.move;
+J_plus.move = ones(battlefield_size,battlefield_size);
 
+t = 0; %value iteration counter
 
+while norm(J.move - J_plus.move) >= 1e-6
+    t = t+1;
+    J.move = J_plus.move;
+    for i = 1:battlefield_size
+        for j = 1:battlefield_size
+            %update PC movement
+            
+        end 
+    end
+end
 
 
 
